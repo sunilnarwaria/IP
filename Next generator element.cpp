89e75a56bbd89b18ef00065024629fe3b1a64176
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2019-12-24 10:52:05 +0530

    Update Next generator element.cpp

--- Next generator element.cpp
+++ Next generator element.cpp
@@ -58,3 +58,50 @@ int main() {
   printNGE(arr, n);
   return 0;
 }
+
+//code by sunil
+#include<bits/stdc++.h>
+using namespace std;
+
+int nge(int arr[],int n)
+{
+    stack <int> s;
+      s.push(arr[0]);
+      for(int i=1;i<n;i++)
+      {
+          if(s.empty())
+          {
+              s.push(arr[i]);
+              continue;
+          }
+
+
+      while(s.empty()==false && arr[i]>s.top() )
+      {
+          cout<<s.top()<<"-->"<<arr[i]<<endl;
+          s.pop();
+      }
+      s.push(arr[i]);
+      }
+      while(s.empty()== false)
+      {
+          cout<<s.top()<<"-->"<<-1<<endl;
+          s.pop();
+      }
+
+}
+int main()
+{
+
+    int arr[100];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+    nge(arr,n);
+    return 0;
+}
+
